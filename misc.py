06ed5a71d52e6582243c1c09a0490d2b61b1650f
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-07 13:28:48 +0530

    sa

---
+++ misc.py
@@ -0,0 +1,19 @@
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+
+
+logging.basicConfig(level=logging.INFO)
+API_TOKEN = "Your api token"
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN, parse_mode=types.ParseMode.HTML)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+dp.middleware.setup(LoggingMiddleware())
